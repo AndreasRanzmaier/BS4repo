d4ab1bd5c409bc5beaa6ef0d43693a8170f1b3f2
RanzmaierAndreas <a.ranzmaier@barbaric.at>
2023-05-02 15:15:54 +0200

    added bootstrap dark mode

--- INF_ITL/PHP/01/base.php
+++ INF_ITL/PHP/01/base.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrierung</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
 <?php
+//          servername   db-name   db-user db-password
 $db = conn("localhost", "TestDB", "root", "4724");
 function conn($host, $dbname, $user, $password)
 {
@@ -27,7 +28,7 @@ print_r(select($db));
 ?>
 
 <body>
-
+    <P>SOME TEXT</P>
 
 
     <!-- js librarys -->
