ac596b97ded5677c53cba2b3e5caa099ba208d48
RanzmaierAndreas <a.ranzmaier@barbaric.at>
2023-05-22 15:49:14 +0200

    yes

--- INF_ITL/PHP/01/base.php
+++ INF_ITL/PHP/01/base.php
@@ -4,13 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Registrierung</title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
+<!-- Database connection -->
 <?php
 //          servername   db-name   db-user db-password
-$db = conn("localhost", "TestDB", "root", "4724");
+$db = conn("localhost", "TesadatDB", "root", "4724");
 function conn($host, $dbname, $user, $password)
 {
     return new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $user, $password);
@@ -27,7 +30,7 @@ function select($db, $selectString)
 <body>
     <P>SOME TEXT</P>
 
-    <table>
+    <table class="table table-dark table-borderless">
         <tr>
             <th>TestID</th>
             <th>name</th>
