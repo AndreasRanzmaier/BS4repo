7e3a2eb265e8e83fe1ecaaee4c0d78596d5b7b4d
RanzmaierAndreas <a.ranzmaier@barbaric.at>
2023-06-12 14:09:29 +0200

    deleted unnecessary function

--- INF_ITL/PHP/Vorlage/functions.php
+++ INF_ITL/PHP/Vorlage/functions.php
@@ -1,34 +1,5 @@
 <?php
 $con;
-
-function makeTable($query)
-{
-    global $con;
-    try {
-        $stmt = $con->prepare($query);
-        $stmt->execute();
-        $meta = array();
-        echo '<table class="table">
-            <tr>';
-        $colCount = $stmt->columnCount();
-        for ($i = 0; $i < $colCount; $i++) {
-            $meta[] = $stmt->getColumnMeta($i);
-            echo '<th>' . $meta[$i]['name'] . '</th>';
-        }
-        echo '</tr>';
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo '<tr>';
-            foreach ($row as $r) {
-                echo '<td>' . $r . '</td>';
-            }
-            echo '</tr>';
-        }
-        echo '</table>';
-    } catch (Exception $e) {
-        echo 'Error - Tabelle Adressen: ' . $e->getCode() . ': ' . $e->getMessage() . '<br>';
-    }
-}
-
 function select($db, $selectString)
 {
     $select = $db->prepare($selectString);
